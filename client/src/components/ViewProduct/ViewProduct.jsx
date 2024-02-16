4ab9b3f99abb1637c6ab52c101326027432f2c38
Ilhem-cherif <cherifilhem17@gmail.com>
2024-02-16 23:54:54 +0100

    modification frontend

--- client/src/components/ViewProduct/ViewProduct.jsx
+++ client/src/components/ViewProduct/ViewProduct.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams ,Link} from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { toast } from "react-toastify";
+import {useSelector } from 'react-redux';
+import { useParams ,Link, useNavigate} from 'react-router-dom';
 import './ViewProduct.css'; 
 const ViewProduct = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn); // Get login state
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -18,7 +25,51 @@ const ViewProduct = () => {
 
     fetchProduct();
   }, [id]);
-
+  const handleRentNow = async () => {
+    try {
+      // Check if user is logged in
+      if (!isLoggedIn) {
+        navigate('/login'); // Redirect to login page if not logged in
+        return;
+      }
+      if(!startDate || !endDate){
+        toast.error('Please select both start and end dates', {
+          position: "bottom-left"
+      });
+      }
+      // Format dates to ISO 8601 format
+      const fromISO = startDate.toISOString();
+      const toISO = endDate.toISOString();
+  
+      const response = await axios.post(`http://localhost:3001/api/rental/${product._id}`, {
+        bookedTimeSlots: {
+          from: fromISO,
+          to: toISO
+        }
+      });
+  
+      // Check if the rental was created successfully (status code 201)
+      if (response.status === 201) {
+        toast.success('Rental created successfully', {
+            position: "bottom-left"
+        });
+      } else {
+        toast.error(response.data.message, {
+            position: "bottom-left"
+        });
+      }
+    } catch (error) {
+        if (error.response && error.response.data && error.response.data.message) {
+        toast.error(error.response.data.message, {
+            position: "bottom-left"
+        });
+        } else {
+        toast.error("An error occurred while processing your request.", {
+            position: "bottom-left"
+        });
+    }
+  }
+  };
   if (!product) {
     return <div>Loading...</div>;
   }
@@ -39,17 +90,41 @@ const ViewProduct = () => {
       <div className="product-details">
         <h1 className="product-title">{product.Title}</h1>
         <p className="product-info">Description: {product.description}</p>
-        <p className="product-info">Quantity: {product.quantity}</p>
         <p className="product-info"> <span className="product-info1">${product.rentPerHour}</span>/ Hour</p>
         <p className="product-info"><span className="product-info1">${product.rentPerDay}</span>/ Day </p>
         <p className="product-info">
         Category: <Link to={`/products/byCategory/${product.category}`} >{product.categoryName}</Link>
       </p>
-      <Link to={`/rent/${product._id}`}>
-            <button className="rent-button">Rent Now</button>
-          </Link> 
-   </div>
-    </div>
+      <div className="date-picker-container">
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="From Date"
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="MM/dd/yyyy h:mm aa"
+            />
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              placeholderText="To Date"
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="MM/dd/yyyy h:mm aa"
+            />
+          </div>
+          <button onClick={handleRentNow} className="rent-button">Rent Now</button> 
+        </div>
+      </div>
     </div>
   );
 };
